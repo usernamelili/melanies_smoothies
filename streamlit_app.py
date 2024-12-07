0b30bbb82bae123e54ad1ab2b7ac06c2bc2a7d63
usernamelili <usernamelili@users.noreply.github.com>
2024-12-08 08:49:06 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.write("The name on your smoothie will be: ", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop() # to focus on this section
 
 # convert snowpak dataframe to panda datafrem to use LOC
