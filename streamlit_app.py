57592c08863e3f58c6035c7d4d5863d50f0d974e
usernamelili <usernamelili@users.noreply.github.com>
2024-12-08 08:52:26 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ ingredients_list=st.multiselect(
     'Choose up to 5 ingredients:'
     ,my_dataframe
     ,max_selections=5
-)
+    )
 
 if ingredients_list: 
     # st.write(ingredients_list)
@@ -40,7 +40,7 @@ if ingredients_list:
         ingredients_string += fruit_chosen +' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', SEARCH_ON, '.')
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen +'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
